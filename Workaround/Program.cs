7360bb0712201b974533839b7ff25a7c8f0a13ec
Emre Tekbaş <emretekbas97@outlook.com>
2024-02-11 03:24:57 +0300

    tc silindi

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -3,7 +3,7 @@ using Business.Concrete;
 using Entities.Concrete;
 
 Person fake_citizen = new() { FirstName = "Emre", LastName = "Testingoğulları", NatiaonalIdentity = 12345678910, DateOfBirthYear = 1990 };
-Person hasso_citizen = new() { FirstName = "Emre", LastName = "Testingoğulları", NatiaonalIdentity = 12345678910, DateOfBirthYear = 1990 };
+Person hasso_citizen = new() { FirstName = "Emre", LastName = "Tekbaş", NatiaonalIdentity = 00000000000, DateOfBirthYear = 1997 };
 
 new PTTManager(new CitizenManager()).GiveMask(fake_citizen);
 
